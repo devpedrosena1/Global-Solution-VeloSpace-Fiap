403c4c7216321c8ea3c7f44bb3c1f3511e23f03e
devpedrosena1 <devpedrosena1@gmail.com>
2026-05-26 23:13:17 -0300

    criando projeto spring, adicionando dependencias e configurando application.yml para uso de banco de dados Oracle

---
+++ VeloSpace/src/main/java/br/com/fiap/javaadv/VeloSpace/VeloSpaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javaadv.VeloSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloSpaceApplication.class, args);
+	}
+
+}
---
+++ VeloSpace/src/test/java/br/com/fiap/javaadv/VeloSpace/VeloSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javaadv.VeloSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
