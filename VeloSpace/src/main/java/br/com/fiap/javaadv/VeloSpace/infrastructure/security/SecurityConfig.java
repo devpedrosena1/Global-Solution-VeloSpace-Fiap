2818259064012e3e41f1efda4c84898dbec47523
PauloSergioFB <paulosergiofb2002@gmail.com>
2026-06-09 06:45:47 -0300

    feat: new test endpoint

--- VeloSpace/src/main/java/br/com/fiap/javaadv/VeloSpace/infrastructure/security/SecurityConfig.java
+++ VeloSpace/src/main/java/br/com/fiap/javaadv/VeloSpace/infrastructure/security/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                                 "/",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
-                                "/swagger-ui/**")
+                                "/swagger-ui/**",
+                                "/api/v1/test")
                         .permitAll()
 
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth")
---
+++ VeloSpace/src/main/java/br/com/fiap/javaadv/VeloSpace/presentation/controllers/TestController.java
@@ -0,0 +1,20 @@
+package br.com.fiap.javaadv.VeloSpace.presentation.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Test Controller", description = "Controller de teste para verificar se a aplicação está funcionando corretamente")
+public class TestController {
+
+    @GetMapping("/test")
+    @Operation(summary = "Teste", description = "Endpoint de teste para verificar se a aplicação está funcionando corretamente")
+    public String test() {
+        return "Testando o controller";
+    }
+
+}
